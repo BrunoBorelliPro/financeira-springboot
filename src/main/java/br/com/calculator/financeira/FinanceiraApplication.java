48b59bc0cddf0ef380fe0a2f09428b8f57f719ab
Bruno Borelli <brunoborellipro@gmail.com>
2021-04-16 14:44:06 -0300

    Commit inicial

---
+++ src/main/java/br/com/calculator/financeira/FinanceiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.calculator.financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiraApplication.class, args);
+	}
+
+}
